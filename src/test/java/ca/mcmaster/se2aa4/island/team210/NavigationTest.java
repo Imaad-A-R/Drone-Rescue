aba364276c35e5b87c0222e137ffc981cca34efe
Cemal Enes Durak <durakc@mcmaster.ca>
2024-02-19 07:57:54 -0500

    Added unit test for Navigation Class

---
+++ src/test/java/ca/mcmaster/se2aa4/island/team210/NavigationTest.java
@@ -0,0 +1,16 @@
+package ca.mcmaster.se2aa4.island.team210;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NavigationTest {
+    private Navigation navigator = new Navigation();
+    private Map m = new Map(new Drone(7000));
+    @Test
+    public void makeADecisionFindIslandTest(){
+        String[] test_array = navigator.makeADecision(m);
+        assertEquals("fly", test_array[0]);
+    }
+}
