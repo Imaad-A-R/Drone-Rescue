0366166e986677b58fc629aeb0f1648b43d01ee9
Cemal Enes Durak <durakc@mcmaster.ca>
2024-02-29 10:53:50 -0500

    Added unit test case for Drone Class close #30

--- src/test/java/ca/mcmaster/se2aa4/island/team210/DroneTest.java
+++ src/test/java/ca/mcmaster/se2aa4/island/team210/DroneTest.java
@@ -17,5 +17,10 @@ public class DroneTest {
         aDrone.removeCost(87);
         assertEquals(6859, aDrone.getBattery());
     }
-
+    
+    @Test
+    public void handleDirectionTest(){
+        aDrone.handleDirection("N");
+        assertEquals("N", aDrone.getDirection());
+    }
 }
