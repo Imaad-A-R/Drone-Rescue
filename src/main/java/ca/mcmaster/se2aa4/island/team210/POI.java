de9da5bc0fd75d4d16949e1cd7c8e2a6688769c5
Imaad Abdul Razzak <imaadar@gmail.com>
2024-03-11 18:02:44 -0400

    Made POI objects store the id of the poi (close #58)

--- src/main/java/ca/mcmaster/se2aa4/island/team210/POI.java
+++ src/main/java/ca/mcmaster/se2aa4/island/team210/POI.java
@@ -5,9 +5,11 @@ import java.util.List;
 public class POI {
     private final Integer xCoord;
     private final Integer yCoord;
+    private final String id;
 
-    POI(Integer[] currentCoords){
+    POI(Integer[] currentCoords, String id){
         this.xCoord = currentCoords[0];
         this.yCoord = currentCoords[1];
+        this.id = id;
     }
 }
